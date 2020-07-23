7ca7ac4d4d735b72e7125bf4ca23b869fddc62c3
Jiya Wadhwa <jiya.wadhwa@Beings-MacBook-Air.local>
2020-07-23 17:07:08 +0530

    Adding HTML Page index.html

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(__dirname+'/public/html/index.html');
+})
+
+app.get('/:page', (req, res) => {
+    res.sendFile(__dirname+'/public/html/'+req.params.page +".html");
 })
 
 var port = process.env.PORT || 3000;
